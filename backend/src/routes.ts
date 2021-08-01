170bcaf4db92102170f719942cb9d3838a53e461
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-01 17:23:22 -0300

    ➕ add uuid

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,10 +2,22 @@ import { Router, Request, Response } from 'express'
 
 const routes = Router()
 
-routes.get('/', (request: Request, response: Response) => {
-	return response.json({
-		message: 'hello world',
-	})
+interface User {
+	id: string
+	name: string
+	email: string
+}
+
+const users: User[] = []
+
+routes.get('/users', (request: Request, response: Response) => {
+	return response.json(users)
+})
+
+routes.post('/users', (request: Request, response: Response) => {
+	const { name, email } = request.body
+
+	const user = { id: '', name }
 })
 
 export default routes
