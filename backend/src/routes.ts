880b2f672c090dcb0f44dd029645919877437e79
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-01 17:42:51 -0300

    ✨ add basic crud

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express'
+import { v4 as uuid } from 'uuid'
 
 const routes = Router()
 
@@ -17,7 +18,44 @@ routes.get('/users', (request: Request, response: Response) => {
 routes.post('/users', (request: Request, response: Response) => {
 	const { name, email } = request.body
 
-	const user = { id: '', name }
+	const user = { id: uuid(), name, email }
+
+	users.push(user)
+	return response.json(user).status(201)
+})
+
+routes.put('/users/:id', (request: Request, response: Response) => {
+	const { id } = request.params
+	const { name, email } = request.body
+
+	const userIndex = users.findIndex((user) => user.id === id)
+
+	if (userIndex < 0) {
+		return response.status(404).json({
+			error: 'User not found',
+		})
+	}
+
+	const user = { id, name, email }
+	users[userIndex] = user
+	return response.json(user)
+})
+
+routes.delete('/users/:id', (request: Request, response: Response) => {
+	const { id } = request.params
+	const { name, email } = request.body
+
+	const userIndex = users.findIndex((user) => user.id === id)
+
+	if (userIndex < 0) {
+		return response.status(404).json({
+			error: 'User not found',
+		})
+	}
+
+	users.splice(userIndex, 1)
+
+	return response.status(204).send()
 })
 
 export default routes
