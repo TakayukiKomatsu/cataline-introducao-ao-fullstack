5b2604137c6f8221c02f8e1c3d01f18f3458b911
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-01 18:23:52 -0300

    💄 add settings to prettierrc

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,17 @@
 <template>
-	<h1>Hello world</h1>
+	<h1>{{ message }}</h1>
 </template>
 
 <script lang="ts">
 	import { defineComponent } from 'vue'
 
-	export default defineComponent({})
+	export default defineComponent({
+		data() {
+			return {
+				message: 'Hello world',
+			}
+		},
+	})
 </script>
 
 <style></style>
