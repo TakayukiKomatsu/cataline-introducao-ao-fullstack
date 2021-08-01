936ba551ed84fa66fe2963b5fca4f491bca3e03f
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-01 18:53:06 -0300

    ➕ add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express'
 import routes from './routes'
 
+import cors from 'cors'
+
 const app = express()
 
 app.use(express.json())
+app.use(
+	cors({
+		origin: '*',
+	})
+)
+
 app.use(routes)
 
 app.listen('3333', () => {
